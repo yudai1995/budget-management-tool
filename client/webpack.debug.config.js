fee8d70570657848798f62eaa81200c8e6ee5813
yudaiy <y.yamamoto991103@gmail.com>
2022-09-18 18:59:35 +0900

    add debug setting for react

---
+++ client/webpack.debug.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'debug',
+    devtool: 'inline-source-map',
+    devServer: {
+        index: 'index.html',
+        port: 9223,
+        historyApiFallback: true,
+    },
+};
