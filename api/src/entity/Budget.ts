19edae527615f6b2e9b1c84003beabe3d52595cb
yamamoto <y.yamamoto991103@gmail.com>
2025-01-13 05:22:25 +0900

    fix settings

--- api/src/entity/Budget.ts
+++ api/src/entity/Budget.ts
@@ -1,41 +1,33 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  UpdateDateColumn,
-  DeleteDateColumn,
-  CreateDateColumn,
-  PrimaryColumn,
-} from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity({
-  name: 'budget_list',
+    name: 'budget_list',
 })
 export class Budget {
-  @PrimaryColumn({ type: 'varchar' })
-  id: string;
+    @PrimaryColumn({ type: 'varchar' })
+    id: string
 
-  @Column({ type: 'int' })
-  amount: number;
+    @Column({ type: 'int' })
+    amount: number
 
-  @Column({ enum: [0, 1] })
-  balanceType: 0 | 1;
+    @Column({ type: 'enum', enum: [0, 1] })
+    balanceType: 0 | 1
 
-  @Column({ default: 1 })
-  categoryId: number;
+    @Column({ default: 1 })
+    categoryId: number
 
-  @Column({ nullable: true })
-  content: string;
+    @Column({ nullable: true })
+    content: string
 
-  @Column()
-  date: string;
+    @Column()
+    date: string
 
-  @CreateDateColumn({ update: false })
-  createdDate: Date;
+    @CreateDateColumn({ update: false })
+    createdDate: Date
 
-  @UpdateDateColumn()
-  updatedDate: Date;
+    @UpdateDateColumn()
+    updatedDate: Date
 
-  @DeleteDateColumn()
-  deletedDate: Date;
+    @DeleteDateColumn()
+    deletedDate: Date
 }
